f984070da13149e3266906a73138ebbcfae96750
Jesús Germade <jesus@germade.es>
2015-05-01 09:21:15 +0200

    fixing node definition

--- scope.js
+++ scope.js
@@ -2,19 +2,19 @@
  * css.js
  *
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2014 Jesús Manuel Germade Castiñeiras <jesus@germade.es>
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,26 +22,24 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- * 
+ *
  */
 
 
-(function (definition) {
+(function (root, definition) {
 	'use strict';
-	
-	if ( typeof window === 'undefined' ) {
-		if ( typeof module !== 'undefined' ) {
-			module.exports = definition();
-		}
+
+	if ( typeof module !== 'undefined' ) {
+		module.exports = definition();
 	} else {
-		if ( window.fn ) {
-			fn.define('Scope', definition );
-		} else if( !window.Scope ) {
-			window.Scope = definition();
+		if ( root.define ) {
+			define('Scope', definition );
+		} else if( !root.Scope ) {
+			root.Scope = definition();
 		}
 	}
 
-})(function () {
+})(this, function () {
 	'use strict';
 
     function parseExpression (expression) {
